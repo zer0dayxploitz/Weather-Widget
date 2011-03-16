6ed1417889a31982948c5e8dd01a07521ab613d6
Zach <zs3223@gmail.com>
2011-03-15 22:11:29 -0400

    Updated the configuration activity.

--- src/com/zer0day/weather/widget/WeatherWidgetConfig.java
+++ src/com/zer0day/weather/widget/WeatherWidgetConfig.java
@@ -20,59 +20,58 @@ public class WeatherWidgetConfig extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-
-	setResult(RESULT_CANCELED);
-
-	 setContentView(R.layout.config);
-
-	 configOkButton = (Button)findViewById(R.id.okconfig);
-	 configOkButton.setOnClickListener(configOkButtonOnClickListener);
-
-	 Intent intent = getIntent();
-	 Bundle extras = intent.getExtras();
-	 if (extras != null) {
-	     mAppWidgetId = extras.getInt(
-	             AppWidgetManager.EXTRA_APPWIDGET_ID,
-	             AppWidgetManager.INVALID_APPWIDGET_ID);
-	 }
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	
+		setResult(RESULT_CANCELED);
+	
+		setContentView(R.layout.config);
+		
+		configOkButton = (Button)findViewById(R.id.saveconfig);
+		configOkButton.setOnClickListener(configOkButtonOnClickListener);
+		
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+		    mAppWidgetId = extras.getInt(
+		            AppWidgetManager.EXTRA_APPWIDGET_ID,
+		            AppWidgetManager.INVALID_APPWIDGET_ID);
+		}
 
 	 // If they gave us an intent without the widget id, just bail.
 	 if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
-	     finish();
-	 }
+	     	finish();
+	 	}
 	}
-
 
 private Button.OnClickListener configOkButtonOnClickListener = new Button.OnClickListener(){
 
-@Override
-public void onClick(View arg0) {
-// TODO Auto-generated method stub
-
-final Context context = WeatherWidgetConfig.this;
-
-AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-
-//RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.hellowidget_layout);
-//appWidgetManager.updateAppWidget(mAppWidgetId, views);
-WeatherWidgetProvider.updateAppWidget(context, appWidgetManager, mAppWidgetId);
-
-Toast.makeText(context, "HelloWidgetConfig.onClick(): " + String.valueOf(mAppWidgetId) , Toast.LENGTH_LONG).show();
-
-//prepare Alarm Service to trigger Widget
-   Intent intent = new Intent(WeatherWidgetProvider.MY_WIDGET_UPDATE);
-   PendingIntent pendingIntent = PendingIntent.getBroadcast(WeatherWidgetConfig.this, 0, intent, 0);
-   AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-   Calendar calendar = Calendar.getInstance();
-   calendar.setTimeInMillis(System.currentTimeMillis());
-   calendar.add(Calendar.SECOND, 10);
-   alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 20*1000, pendingIntent);
-
-Intent resultValue = new Intent();
-resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-setResult(RESULT_OK, resultValue);
-finish();
-}};
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			
+			final Context context = WeatherWidgetConfig.this;
+			
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+			
+			//RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.hellowidget_layout);
+			//appWidgetManager.updateAppWidget(mAppWidgetId, views);
+			WeatherWidgetProvider.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+			
+			Toast.makeText(context, "HelloWidgetConfig.onClick(): " + String.valueOf(mAppWidgetId) , Toast.LENGTH_LONG).show();
+			
+			//prepare Alarm Service to trigger Widget
+			Intent intent = new Intent(WeatherWidgetProvider.MY_WIDGET_UPDATE);
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(WeatherWidgetConfig.this, 0, intent, 0);
+			AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTimeInMillis(System.currentTimeMillis());
+			calendar.add(Calendar.SECOND, 10);
+			alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 20*1000, pendingIntent);
+			
+			Intent resultValue = new Intent();
+			resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+			setResult(RESULT_OK, resultValue);
+			finish();
+		}};
 }
